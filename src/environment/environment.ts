49850b40980789ff157603ca32f5442f74a9f8d4
Gabriel Llamas <gabrielllamasllopis@storyteq.com>
2026-01-29 14:04:58 +0100

    update page title

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -4,5 +4,3 @@ export const environment: AppEnvironment = {
   VITE_CARTO_API_TOKEN: import.meta.env.VITE_CARTO_API_TOKEN,
   VITE_CARTO_API_BASE_URL: import.meta.env.VITE_CARTO_API_BASE_URL,
 } as const;
-
-console.log(environment)
